31ff76807b62bd09d1893d3ea11d3d316815d1c5
Victor <victor.silveira@sptech.school>
2022-04-17 15:20:23 -0300

    Modo dark

--- SiteInstitucionalDarkMode/js/changeScreens.js
+++ SiteInstitucionalDarkMode/js/changeScreens.js
@@ -2,7 +2,11 @@
 
     function changeScreenSingIn() {
         window.location.href = "login.html";
+       
     }
     function changeScreenSingUp() {
         window.location.href = "signUp.html";
+    }
+    function changeDashboard() {
+        window.location.href = "dashboard.html";
     }
