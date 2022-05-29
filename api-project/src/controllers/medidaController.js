938d8d6db8350ac42c61c46bb62ca3ccb49f21e8
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-05-29 20:48:06 -0300

    Creating Grafic's in Racks

--- api-project/src/controllers/medidaController.js
+++ api-project/src/controllers/medidaController.js
@@ -1,94 +1,136 @@
 const medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
+  const limite_linhas = 7;
 
-    const limite_linhas = 7;
+  var idAquario = req.params.idAquario;
 
-    var idAquario = req.params.idAquario;
+  console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
-
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+  medidaModel
+    .buscarUltimasMedidas(idAquario, limite_linhas)
+    .then(function (resultado) {
+      if (resultado.length > 0) {
+        res.status(200).json(resultado);
+      } else {
+        res.status(204).send("Nenhum resultado encontrado!");
+      }
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as ultimas medidas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
     });
 }
 
-
 function buscarMedidasEmTempoReal(req, res) {
+  var idAquario = req.params.idAquario;
 
-    var idAquario = req.params.idAquario;
+  console.log(`Recuperando medidas em tempo real`);
 
-    console.log(`Recuperando medidas em tempo real`);
-
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+  medidaModel
+    .buscarMedidasEmTempoReal(idAquario)
+    .then(function (resultado) {
+      if (resultado.length > 0) {
+        res.status(200).json(resultado);
+      } else {
+        res.status(204).send("Nenhum resultado encontrado!");
+      }
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as ultimas medidas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
     });
 }
 
 function buscarMetricas(req, res) {
-    const idEmpresa = req.body.MyEmpresa;
-    const idRack = req.body.MyRack;
+  const idEmpresa = req.body.MyEmpresa;
+  const idRack = req.body.MyRack;
 
-    medidaModel.buscarMetricas(idEmpresa, idRack).then((response) => {
-        const resultado = response.length;
+  medidaModel
+    .buscarMetricas(idEmpresa, idRack)
+    .then((response) => {
+      const resultado = response.length;
 
-        if (resultado > 0) {
-            res.status(200).json({
-                response
-            })
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch((erro) => {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+      if (resultado > 0) {
+        res.status(200).json({
+          response,
+        });
+      } else {
+        res.status(204).send("Nenhum resultado encontrado!");
+      }
+    })
+    .catch((erro) => {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as ultimas medidas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
     });
 }
 
 function buscarMetricasTempoReal(req, res) {
-    const idEmpresa = req.body.idEmpresa;
-    const idRack = req.body.idRack;
+  const idEmpresa = req.body.idEmpresa;
+  const idRack = req.body.idRack;
 
-    console.log(`Recuperando medidas em tempo real`);
+  console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMetricasTempoReal(idEmpresa, idRack).then((response) => {
-        const resultado = response.length;
+  medidaModel
+    .buscarMetricasTempoReal(idEmpresa, idRack)
+    .then((response) => {
+      const resultado = response.length;
 
-        if (resultado > 0) {
-            res.status(200).json({
-                response
-            })
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as medidas atualizadas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+      if (resultado > 0) {
+        res.status(200).json({
+          response,
+        });
+      } else {
+        res.status(204).send("Nenhum resultado encontrado!");
+      }
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as medidas atualizadas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
     });
 }
-  
+
+function buscarMaxMin(req, res) {
+  const idEmpresa = req.body.idEmpresa;
+  const idRack = req.body.idRack;
+
+  medidaModel
+    .buscarMaxMin(idEmpresa, idRack)
+    .then((response) => {
+      res.json({
+        response,
+      });
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as medidas atualizadas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
+    });
+}
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMetricas,
-    buscarMedidasEmTempoReal,
-    buscarMetricasTempoReal,
-}
+  buscarUltimasMedidas,
+  buscarMetricas,
+  buscarMedidasEmTempoReal,
+  buscarMetricasTempoReal,
+  buscarMaxMin,
+};
--- api-project/src/models/medidaModel.js
+++ api-project/src/models/medidaModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
+  instrucaoSql = `select 
                         temperatura, 
                         umidade, 
                         momento,
@@ -9,40 +9,51 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     from medida
                     where fk_aquario = ${idAquario}
                     order by id desc limit ${limite_linhas}`;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
+  instrucaoSql = `select 
                         temperatura, 
                         umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
                         fk_aquario 
                         from medida where fk_aquario = ${idAquario} 
                     order by id desc limit 1`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
-function buscarMetricas(idEmpresa,idRack){
-    const query = `select metrica.temperatura, metrica.umidade, metrica.date_time from empresa join metrica on metrica.fk_empresa = empresa.id join sensor on sensor.id = metrica.fk_sensor where empresa.id = ${idEmpresa} and sensor.rack = ${idRack}`;
+function buscarMetricas(idEmpresa, idRack) {
+  const query = `SELECT M.temperatura, M.umidade, DATE_FORMAT(M.date_time,'%H:%i:%s') as momento_grafico FROM metrica AS M 
+    JOIN sensor AS S ON S.id = M.fk_sensor 
+    WHERE M.fk_empresa = ${idEmpresa} AND S.rack = ${idRack} ORDER BY M.date_time ASC`;
 
-    return database.executar(query);
-
+  return database.executar(query);
 }
 function buscarMetricasTempoReal(idEmpresa, idRack) {
-    const query = `SELECT * FROM empresa AS E JOIN usuario AS U on U.fk_empresa = E.id
-    JOIN metrica AS M ON M.fk_empresa = E.id
-    JOIN sensor AS S ON M.fk_sensor = S.id WHERE E.id = 1 AND S.rack = 1
-    order by S.id desc limit 1`;
+  const query = `SELECT M.temperatura, M.umidade, DATE_FORMAT(M.date_time,'%H:%i:%s') AS momento_grafico FROM metrica AS M 
+  JOIN sensor AS S ON S.id = M.fk_sensor 
+  WHERE M.fk_empresa = ${idEmpresa} AND S.rack = ${idRack} ORDER BY M.date_time DESC LIMIT 1`;
 
-    return database.executar(query);
+  return database.executar(query);
+}
+
+function buscarMaxMin(idEmpresa, idRack) {
+  const query = `SELECT MAX(M.temperatura) AS 'maximo_temp', 
+  MIN(M.temperatura) AS 'minimo_temp', MAX(M.umidade) AS 'maximo_umid', 
+  MIN(M.umidade) AS 'minimo_umid' FROM metrica AS M 
+  JOIN sensor AS S ON S.id = M.fk_sensor 
+  WHERE M.fk_empresa = ${idEmpresa} AND S.rack = ${idRack} LIMIT 1`;
+
+  return database.executar(query);
 }
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal,
-    buscarMetricas,
-    buscarMetricasTempoReal
-}
+  buscarUltimasMedidas,
+  buscarMedidasEmTempoReal,
+  buscarMetricas,
+  buscarMetricasTempoReal,
+  buscarMaxMin,
+};
